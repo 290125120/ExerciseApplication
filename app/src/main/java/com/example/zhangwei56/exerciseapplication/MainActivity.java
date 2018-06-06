4742aac6f8850ccba174c0fbaa6df8a4f3a1dfbb
zhangwei56 <zhangwei56@jd.com>
2018-06-06 15:50:11 +0800

    第3次提交 lifecycle demo

--- app/src/main/java/com/example/zhangwei56/exerciseapplication/MainActivity.java
+++ app/src/main/java/com/example/zhangwei56/exerciseapplication/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.zhangwei56.exerciseapplication;
 
 
 import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements LifecycleOwner{
+import com.example.zhangwei56.exerciseapplication.ViewModel.UserViewModel;
+
+public class MainActivity extends AppCompatActivity{
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        UserViewModel userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+        userViewModel.getUserLiveData().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                if (user != null) {
+                    textView.setText(user.getName());
+                }
+
+            }
+        });
     }
 }
